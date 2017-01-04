645083612f43b9a46003f9066d9ab6845364db1a
phanikumarp <phanikumarp@users.noreply.github.com>
2017-01-04 16:22:59 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -17,8 +17,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
     public static Map<BadKey,String> leakMap = new HashMap<>();
     
-   
-    
+  
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         //memory leak code  
